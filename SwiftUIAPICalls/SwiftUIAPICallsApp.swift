a475e2cb4fcdd5c2de46f981233c949a8b71e99a
Jhonarias13 <jhonarias1322@gmail.com>
2023-02-07 07:24:25 -0500

    Initial Commit

---
+++ SwiftUIAPICalls/SwiftUIAPICallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAPICallsApp.swift
+//  SwiftUIAPICalls
+//
+//  Created by Jhon Freiman Arias on 7/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAPICallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
